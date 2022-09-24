164738db1bf4b2f8869931bec386d1aeb8ff428a
Lucas Cesar <atomo.lambert_0q@icloud.com>
2022-09-24 19:13:54 -0300

    adicionando tela help

--- src/help/Help.java
+++ src/help/Help.java
@@ -8,36 +8,28 @@ public class Help {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+	public static void launchAppHelp() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Help window = new Help();
 					window.frame.setVisible(true);
+					window.frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	/**
-	 * Create the application.
-	 */
+	
 	public Help() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
